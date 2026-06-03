f56c6c48eaffdec869c4ba21dc5eedea3dafccf8
Christian Abou-Ezzi <christian.abouezzi@gmail.com>
2026-06-02 20:03:37 -0400

    portfolio update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,20 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Christian Abou-Ezzi — Biomedical Engineer",
+  author: "Christian Abou-Ezzi",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    "Biomedical Engineer based in Boston MA, USA. I specialize in Medical Device and Signal Processing.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/christian_large.jpeg",
   navLinks: [
-    { text: "Experience", href: "#experience" },
+    // { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/christian-abou-ezzi-202a50241/#:~:text=www.linkedin.com/in/christian%2Dabou%2Dezzi%2D202a50241"},
+    // { text: "Github", href: "https://github.com/immois/astro-zen" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -25,43 +22,43 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Christian Abou-Ezzi",
+    specialty: "Biomedical Engineer",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+      "Biomedical Engineer based in Boston MA, USA. I specialize in Medical Device and Signal Processing.",
+    email: "christian.abouezzi@gmail.com",
   },
-  experience: [
-    {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
-      summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
-      ],
-    },
-    {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
-      summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
-      ],
-    },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
-    },
-  ],
+  // experience: [
+  //   {
+  //     company: "Zalmart",
+  //     position: "Lead Android Developer",
+  //     startDate: "May 2018",
+  //     endDate: "Sept 2020",
+  //     summary: [
+  //       "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
+  //       "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
+  //       "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+  //     ],
+  //   },
+  //   {
+  //     company: "Bankit",
+  //     position: "Mobile Developer",
+  //     startDate: "Feb 2017",
+  //     endDate: "May 2018",
+  //     summary: [
+  //       "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
+  //       "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
+  //     ],
+  //   },
+  //   {
+  //     company: "Driveer",
+  //     position: "Frontend Developer",
+  //     startDate: "Jun 2015",
+  //     endDate: "Oct 2016",
+  //     summary:
+  //       "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
+  //   },
+  // ],
   projects: [
     {
       name: "Spotifu Music",
@@ -87,11 +84,11 @@ export const SITE_CONTENT: SiteContent = {
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
-
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+      Hi, I’m Christian Abou-Ezzi, a Biomedical Engineer fron Boston University. I'm interested in signal processing, medical devices, and clnical engineering, with hands-on experince in MATLAB, Python, and embedded systems like Arduino. I enjoy working on projects that apply engineering and data analysis to real world biological and medical systems, such as ECG signal procesing, audio filtering, and sensor based device development.
+      
+      I'm currently looking to grow my experince in medical technology, clinical engineering, and biomedical signal processing roles.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/christian_large.jpeg",
   },
 };
 
